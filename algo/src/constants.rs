e8b435f2c58731c2f8ba3ad1e8c24db93325e777
Thomas Trinca <tptrinca@gmail.com>
2022-01-25 17:06:53 +1100

    Tweak constants

--- algo/src/constants.rs
+++ algo/src/constants.rs
@@ -1,14 +1,14 @@
 use crate::structs::Point;
 
-pub const POP_LIMIT: u32 = 400;
-pub const INITIAL_GENE_LENGTH: u32 = 200;
-pub const START: Point = Point { x: 0, y: 0 };
-pub const GOAL: Point = Point { x: 2000, y: 1000 };
-pub const MUTATION_RATE: f32 = 0.5;
-pub const GENERATION_LIMIT: u32 = 25;
+pub const POP_LIMIT: u32 = 200;
+pub const INITIAL_GENE_LENGTH: u32 = 50;
+pub const START: Point = Point { x: 300, y: 300 };
+pub const GOAL: Point = Point { x: 1500, y: 1500 };
+pub const MUTATION_RATE: f32 = 0.4;
+pub const GENERATION_LIMIT: u32 = 30;
 
-pub const WORLD_WIDTH: i32 = 2000;
-pub const WORLD_HEIGHT: i32 = 1000;
+pub const WORLD_WIDTH: i32 = 1600;
+pub const WORLD_HEIGHT: i32 = 1600;
 
-pub const MAX_STEP_MOVEMENT: i32 = 30;
-pub const MIN_STEP_MOVEMENT: i32 = -30;
+pub const MAX_STEP_MOVEMENT: i32 = 40;
+pub const MIN_STEP_MOVEMENT: i32 = -40;
